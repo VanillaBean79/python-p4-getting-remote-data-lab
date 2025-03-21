018f1f1ec254cd777551e726db6ab7bcf7084244
Ruben Salazar <rubensalazar@rocketmail.com>
2025-03-21 11:06:03 -0500

    response.content

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,23 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            # b'[\n  {\n   ...nd"\n  }\n]\n'
+            # breakpoint()
+            return response.content
+        else:
+            return None
+        
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+
+        if response_body:
+            return json.loads(response_body)
+        else:
+            return None
